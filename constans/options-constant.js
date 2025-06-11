027d345176a4e6afd2ba29b6ef106306d0bfcfe2
Charlie_McFly <54240686+Aman-Kalkunov@users.noreply.github.com>
2025-06-11 09:45:44 +0400

    Update options-constant.js

--- constans/options-constant.js
+++ constans/options-constant.js
@@ -19,13 +19,11 @@ const wednesdayTime = [
 ];
 
 // Четверг, кроссфит и пробные тренировки
-const thursdayTime = [["18:00"], ["19:00"], ["20:00"]];
+const thursdayTime = [["10:00"], ["11:00"]];
 
 // Пятница, кроссфит и пробные тренировки
 const fridayTime = [
-  ["9:00", "10:00"],
-  ["18:00", "19:00"],
-  ["20:00"],
+  ["10:00", "11:00"],
 ];
 
 // Выходные дни, кроссфит и пробные тренировки
@@ -134,12 +132,12 @@ const getScheduleAllDay = () => [
   { id: 3, day: days[3], date: `${getScheduleDate(3)}` },
   { id: 4, day: days[4], date: `${getScheduleDate(4)}` },
   { id: 5, day: days[5], date: `${getScheduleDate(5)}` },
-  { id: 6, day: days[6], date: `${getScheduleDate(6)}` },
+  // { id: 6, day: days[6], date: `${getScheduleDate(6)}` },
 ];
 
 const getStretchingDay = () => [
   { id: 1, day: days[1], date: `${getScheduleDate(1)}` },
-  { id: 5, day: days[5], date: `${getScheduleDate(5)}` },
+  // { id: 5, day: days[5], date: `${getScheduleDate(5)}` },
   // { id: 6, day: days[6], date: `${getScheduleDate(6)}` },
 ];
 
